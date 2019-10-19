dac58652384a132b8490e9be76465c555783f0a6
Sean McPherson <git@seanmcp.com>
2019-10-19 08:38:17 -0400

    Add read aloud fab

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const synth = window.speechSynthesis
 const readTrigger = document.getElementById('readTrigger')
+const readAloudButton = document.getElementById('read-aloud__button')
 
 const utterance = new SpeechSynthesisUtterance(
     document.querySelector('section').textContent
@@ -16,3 +17,30 @@ readTrigger.addEventListener('click', () => {
         readTrigger.textContent = 'Stop'
     }
 })
+
+function toggleReadAloudMode(e) {
+    e.stopPropagation()
+    const { body } = document
+    const { readAloudModeIsOn } = body.dataset
+    if (readAloudModeIsOn === 'true') {
+        body.dataset.readAloudModeIsOn = 'false'
+        window.removeEventListener('click', clickListener)
+    } else {
+        body.dataset.readAloudModeIsOn = 'true'
+        window.addEventListener('click', clickListener)
+    }
+}
+
+function clickListener(e) {
+    debugger
+    e.preventDefault()
+
+    const { textContent } = e.target
+    if (textContent) {
+        window.speechSynthesis.cancel()
+        window.speechSynthesis.speak(new SpeechSynthesisUtterance(textContent))
+        document.body.dataset.readAloudIsOn = 'true'
+    }
+}
+
+readAloudButton.addEventListener('click', toggleReadAloudMode)
